a63090d8d6e816a65f49dca8c16865a51486b10c
Matt Hughes <mattyjh90@hotmail.co.uk>
2014-10-25 22:25:41 +0100

    Started retrieving Person data for the Person page.

--- Kitbag.HackManchester/Kitbag.HackWebApplication/Models/PersonViewModel.cs
+++ Kitbag.HackManchester/Kitbag.HackWebApplication/Models/PersonViewModel.cs
@@ -8,6 +8,9 @@ namespace Kitbag.HackWebApplication.Models
     public class PersonViewModel
     {
         public string Name { get; set; }
-
+        public string Group { get; set; }
+        public string Email { get; set; }
+        public string Telephone { get; set; }
+        public string Skype { get; set; }
     }
 }
