557a3afff981b2f988ce25c97378f14432224aed
Lee Englestone <lee.englestone@gmail.com>
2014-10-25 14:01:36 +0100

    Group, Person and dashboard Controller added. Dashboard view added

---
+++ Kitbag.HackManchester/Kitbag.HackWebApplication/Controllers/GroupController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Kitbag.HackWebApplication.Controllers
+{
+    public class GroupController : Controller
+    {
+        // GET: Group
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
