98efc82b2cf55183eebc71a6bbf40ba366d02b0b
unknown <Richard.Hopwood@UKCHAKITO006589.express.findel.co.uk>
2014-10-26 00:59:49 +0100

    Status updates from the DB

--- Kitbag.HackManchester/Kitbag.HackWebApplication/Controllers/HomeController.cs
+++ Kitbag.HackManchester/Kitbag.HackWebApplication/Controllers/HomeController.cs
@@ -1,8 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
+using Kitbag.Database;
+using Kitbag.Domain;
+using Kitbag.HackWebApplication.Models;
 
 namespace Kitbag.HackWebApplication.Controllers
 {
@@ -11,7 +10,10 @@ namespace Kitbag.HackWebApplication.Controllers
         [Authorize]
         public ActionResult Index()
         {
-            return View();
+            var model = new HomeViewModel();
+            var repository = new Repository<Status>(new CwonData());
+            model.Statuses = repository.GetAll();
+            return View(model);
         }
 
         public ActionResult About()
---
+++ Kitbag.HackManchester/Kitbag.HackWebApplication/Models/HomeViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using Kitbag.Domain;
+
+namespace Kitbag.HackWebApplication.Models
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<Status> Statuses { get; set; }
+    }
+}
