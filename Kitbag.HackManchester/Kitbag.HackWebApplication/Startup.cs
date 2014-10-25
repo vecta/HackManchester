8721411edf780453722d1422d2acee9ac9e701a0
Lee Englestone <lee.englestone@gmail.com>
2014-10-25 13:19:16 +0100

    New mvc app with Google and Twitter authentication added

---
+++ Kitbag.HackManchester/Kitbag.HackWebApplication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Kitbag.HackWebApplication.Startup))]
+namespace Kitbag.HackWebApplication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
