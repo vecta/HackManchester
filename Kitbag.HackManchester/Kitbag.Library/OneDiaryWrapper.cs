63d6d16a248742e0fbf398364143a3cadd269524
Lee Englestone <lee.englestone@gmail.com>
2014-10-26 11:10:02 +0000

    Dates fixed

--- Kitbag.HackManchester/Kitbag.Library/OneDiaryWrapper.cs
+++ Kitbag.HackManchester/Kitbag.Library/OneDiaryWrapper.cs
@@ -1,6 +1,7 @@
 ﻿using Kitbag.Domain;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Net;
 using System.Text;
@@ -13,8 +14,6 @@ namespace Kitbag.Library
     {
         public static async Task<List<Appointment>> TodaysAppointments()
         {
-            //var appointments = new List<string>();
-
             string requestUrl = "https://api.onediary.com/v1/events?from=2014-10-26&to=2014-10-27&tzid=Europe/London";
 
             var httpClient = new System.Net.Http.HttpClient();
@@ -23,13 +22,26 @@ namespace Kitbag.Library
 
             var appointments = new JavaScriptSerializer().Deserialize<Appointments>(content);
 
+            foreach (var @event in appointments.events)
+            {
+                if (@event.start.Length >= 20)
+                {
+                    //2014-10-26T09:00:00Z
+                    var newDate = DateTime.ParseExact(@event.start,
+                                  "yyyy-MM-ddTHH:mm:ssZ",
+                                  CultureInfo.InvariantCulture);
+
+                    @event.start = newDate.ToString("dd/MM/yyyy HH:mm:ss");
+                }
+            }
+
             return appointments.events;
         }
     }
 
     public class Appointments
     {
-        public List<Appointment> events {get;set;}
+        public List<Appointment> events { get; set; }
     }
-    
+
 }
